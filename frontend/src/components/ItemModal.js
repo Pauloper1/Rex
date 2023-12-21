3e734bb6f4811d8eb7adc24a590a9112e6e2b1fc
Pauloper1 <paulcrescent.p@gmail.com>
2023-12-22 00:38:03 +0530

    spinner added to ItemModal

--- frontend/src/components/ItemModal.js
+++ frontend/src/components/ItemModal.js
@@ -1,17 +1,33 @@
-import {Text, Image, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@chakra-ui/react";
+import { Text, Image, Spinner, Button, Modal, ModalOverlay, ModalContent, ModalBody, ModalFooter } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
-function ItemModal({ isOpen, onClose, image, desc}) {
+function ItemModal({ isOpen, onClose, image, desc }) {
+    const [showImage, setShowImage] = useState(false);
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowImage(true);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, []);
     return (
         <>
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalBody>
-                        <Image
-                            src={image}
-                            borderRadius='lg'
-                        />
+                        {!showImage ? (
+                            <Spinner
+                                thickness="4px"
+                                speed="0.65s"
+                                emptyColor="gray.200"
+                                color="blue.500"
+                                size="xl"
+                            />
+                        ) : (
+                            <Image src={image} borderRadius="lg" />
+                        )}
                         <Text>{desc}</Text>
                     </ModalBody>
                     <ModalFooter>
